61767cc48857ae0facdafed821e58aca34db3a03
Sergiy Poddubka <poseal@gmail.com>
2023-04-16 15:48:18 +0300

    init spring-boot project

---
+++ src/test/java/ua/com/poseal/springbootlessons/SpringBootLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.poseal.springbootlessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLessonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
