225895dea25c32a2796c03aeea530386f07b1cbc
Sergiy Poddubka <poseal@gmail.com>
2023-04-16 20:15:42 +0300

    @PathVariable @RequestParam

--- src/main/java/ua/com/poseal/springbootlessons/intro/controllers/IntroController.java
+++ src/main/java/ua/com/poseal/springbootlessons/intro/controllers/IntroController.java
@@ -1,14 +1,39 @@
 package ua.com.poseal.springbootlessons.intro.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("intro")
 public class IntroController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String introGet() {
-        return "intro";
+    @GetMapping("index")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("about")
+    public String about() {
+        return "about";
+    }
+    @ResponseBody
+    @GetMapping("users")
+    public String users(@RequestParam long id) {
+        return String.format("user id = %s", id);
+    }
+    @ResponseBody
+    @GetMapping("user/{id:\\d+}")
+    public String userById(@PathVariable("id") long id) {
+        return String.format("user id = %s", id);
+    }
+
+    @ResponseBody
+    @RequestMapping(
+            value = {"user", "user/{id:\\d+}/name/{name}"},
+            params = {"id", "name"},
+            method = RequestMethod.GET)
+    public String userByIdAndName(
+            @PathVariable long id,
+            @PathVariable String name) {
+        return String.format("user name = %s, id = %s", name, id);
     }
 }
